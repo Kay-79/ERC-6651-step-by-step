0ed756a24e93d302d11cab7986579b7e59a9f919
Kay Nguyen <kaynguyen.work@gmail.com>
2024-05-08 17:17:20 +0700

    refactor

--- scripts/createAccount.js
+++ scripts/createAccount.js
@@ -10,46 +10,42 @@ async function main() {
     const tokenAddress = process.env.NFT_ADDRESS;
     //replace with tokenId your minted in scripts/mint.js, logged on the CLI
     const tokenId = 0;
-    const chainID = 97; //goerli
+    const chainID = 97; //bsc testnet
     const initData = "0x";
 
-    // const tx = await registry.createAccount(
-    //     implementation,
-    //     chainID,
-    //     tokenAddress,
-    //     tokenId,
-    //     salt,
-    //     initData
-    // );
-    // const receipt = await tx.wait();
-    // const address = await registry.account(
-    //     implementation,
-    //     chainID,
-    //     tokenAddress,
-    //     tokenId,
-    //     salt
-    // );
-    run("verify:verify", {
-        address: "0x9a320167d6B60E6Ce739C79a6B36246b2233558f",
-        constructorArguments: [],
-    });
-    // if (receipt.status == 1 && address) {
-    //     console.log("Account created successfully at address: ", address);
-    //     console.log("Verifying Account contract on BSC Testnet");
-    //     run("verify:verify", {
-    //         address: address,
-    //         constructorArguments: [
-    //             implementation,
-    //             chainID,
-    //             tokenAddress,
-    //             tokenId,
-    //             salt,
-    //             initData,
-    //         ],
-    //     });
-    // } else {
-    //     console.log("Account creation failed");
-    // }
+    const tx = await registry.createAccount(
+        implementation,
+        chainID,
+        tokenAddress,
+        tokenId,
+        salt,
+        initData
+    );
+    const receipt = await tx.wait();
+    const address = await registry.account(
+        implementation,
+        chainID,
+        tokenAddress,
+        tokenId,
+        salt
+    );
+    if (receipt.status == 1 && address) {
+        console.log("Account created successfully at address: ", address);
+        console.log("Verifying Account contract on BSC Testnet");
+        run("verify:verify", {
+            address: address,
+            constructorArguments: [
+                implementation,
+                chainID,
+                tokenAddress,
+                tokenId,
+                salt,
+                initData,
+            ],
+        });
+    } else {
+        console.log("Account creation failed");
+    }
 }
 
 main().catch((error) => {
